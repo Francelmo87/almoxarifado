4ae064c3e0a5c973575a377ecfb1d1ddb8757d6d
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-19 17:11:51 -0300

    criada as views de update e delete de produtos

--- almox/produto/views.py
+++ almox/produto/views.py
@@ -14,13 +14,6 @@ def produto_list(request):
     return render(request, template_name, context)
 
 
-def produto_detail(request, pk):
-    template_name = 'produto_detail.html'
-    obj = Produto.objects.get(pk=pk)
-    context = {'object': obj}
-    return render(request, template_name, context)
-
-
 def produto_add(request):
     form = ProdutoForm(request.POST or None)
     template_name = 'produto_form.html'
@@ -31,30 +24,27 @@ def produto_add(request):
     context = {'form': form}
     return render(request, template_name, context)
 
-# def eta_um_add(request):
-#     form = EtaUmForm(request.POST or None)
-#     template_name = 'eta_um_add.html'
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
-#     context = {'form': form}
-#     return render(request, template_name, context)
 
-#
-# def eta_um_update(request, pk):
-#     template_name = 'eta_um_update.html'
-#     obj = EtaUm.objects.get(pk=pk)
-#     form = EtaUmForm(request.POST or None, instance=obj)
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
-#     context = {'form': form}
-#     return render(request, template_name, context)
-#
-#
-# def eta_um_delete(request, pk):
-#     obj = EtaUm.objects.get(pk=pk)
-#     obj.delete()
-#     return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
+def produto_update(request, pk):
+    template_name = 'produto_update.html'
+    obj = Produto.objects.get(pk=pk)
+    form = ProdutoForm(request.POST or None, instance=obj)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('produto:produto_list'))
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+def produto_delete(request, pk):
+    obj = Produto.objects.get(pk=pk)
+    obj.delete()
+    return HttpResponseRedirect(reverse('produto:produto_list'))
+
+
+def produto_detail(request, pk):
+    template_name = 'produto_detail.html'
+    obj = Produto.objects.get(pk=pk)
+    context = {'object': obj}
+    return render(request, template_name, context)
