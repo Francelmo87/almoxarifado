a5305bcd73a94b3b64418e5563b9a8047b2eec3c
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-19 17:11:10 -0300

    criada as rotas de update e delete de produtos

--- almox/produto/urls.py
+++ almox/produto/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', v.produto_list, name='produto_list'),
     path('<int:pk>', v.produto_detail, name='produto_detail'),
     path('add/', v.produto_add, name='produto_add'),
+    path('edit/<int:pk>', v.produto_update, name='produto_update'),
+    path('delete/<int:pk>', v.produto_delete, name='produto_delete'),
 ]
