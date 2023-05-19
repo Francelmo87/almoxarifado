2ab7b8285e09b9b1770dd634c72d51160a361078
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-19 11:48:46 -0300

    ajustada a view de adicionar produto com form

--- almox/produto/views.py
+++ almox/produto/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .form import ProdutoForm
 from .models import Produto
 
 
@@ -18,5 +22,39 @@ def produto_detail(request, pk):
 
 
 def produto_add(request):
+    form = ProdutoForm(request.POST or None)
     template_name = 'produto_form.html'
-    return render(request, template_name)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('produto:produto_list'))
+    context = {'form': form}
+    return render(request, template_name, context)
+
+# def eta_um_add(request):
+#     form = EtaUmForm(request.POST or None)
+#     template_name = 'eta_um_add.html'
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
+#     context = {'form': form}
+#     return render(request, template_name, context)
+
+#
+# def eta_um_update(request, pk):
+#     template_name = 'eta_um_update.html'
+#     obj = EtaUm.objects.get(pk=pk)
+#     form = EtaUmForm(request.POST or None, instance=obj)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
+#     context = {'form': form}
+#     return render(request, template_name, context)
+#
+#
+# def eta_um_delete(request, pk):
+#     obj = EtaUm.objects.get(pk=pk)
+#     obj.delete()
+#     return HttpResponseRedirect(reverse('eta_um:eta_um_list'))
