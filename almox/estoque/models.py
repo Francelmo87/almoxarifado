0ad1f470a5e43ceb374a44a8c43c2774068f45b6
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-22 10:12:54 -0300

    ajsutes

--- almox/estoque/models.py
+++ almox/estoque/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse_lazy
+
 from almox.base.models import TimeStampedModel
 from almox.produto.models import Produto
 
@@ -22,13 +24,16 @@ class Estoque(TimeStampedModel):
     def __str__(self):
         return f"{self.pk} - {self.nf} - {self.created.strftime('%d-%m-%Y')}"
 
+    def get_absolute_url(self):
+        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk': self.pk})
+
     def nf_formated(self):
         return str(self.nf).zfill(3)
 
 
 class EstoqueItens(models.Model):
-    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE, related_name='estoques')
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='produtos')
     quantidade = models.PositiveIntegerField()
     saldo = models.PositiveIntegerField()
 
