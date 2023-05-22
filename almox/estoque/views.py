8de7e3201b619fa4ebdf3e82bc31ea4435ab0ab8
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-22 10:11:47 -0300

    Criada a view do estoque_entrada_detail

--- almox/estoque/views.py
+++ almox/estoque/views.py
@@ -6,5 +6,12 @@ from .models import Estoque
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
     objects = Estoque.objects.filter(movimento='e')
-    context = {'object_list':objects}
-    return render(request, template_name, context )
+    context = {'object_list': objects}
+    return render(request, template_name, context)
+
+
+def estoque_entrada_detail(request, pk):
+    template_name = 'estoque_entrada_detail.html'
+    obj = Estoque.objects.get(pk=pk)
+    context = {'object': obj}
+    return render(request, template_name, context)
