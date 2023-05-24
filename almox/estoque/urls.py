0f75aa71f97e9582f83f697e6fd7dd540d7099ed
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-24 16:42:31 -0300

    criada a rota para entrada no estoque

--- almox/estoque/urls.py
+++ almox/estoque/urls.py
@@ -5,8 +5,6 @@ app_name = 'estoque'
 
 urlpatterns = [
     path('', v.estoque_entrada_list, name='estoque_entrada_list'),
-    path('<int:pk>', v.estoque_entrada_detail, name='estoque_entrada_detail'),
-    #     path('add/', v.estoque_add, name='estoque_add'),
-    #     path('edit/<int:pk>', v.estoque_update, name='estoque_update'),
-    #     path('delete/<int:pk>', v.estoque_delete, name='estoque_delete'),
+    path('<int:pk>/', v.estoque_entrada_detail, name='estoque_entrada_detail'),
+    path('add/', v.estoque_entrada_add, name='estoque_entrada_add'),
 ]
