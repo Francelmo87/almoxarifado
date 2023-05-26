96bc3163fc22a5ecd7571b2dcff95953ffc72c20
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-26 17:37:38 -0300

    criada a view para lista de saida de estoque

--- almox/estoque/views.py
+++ almox/estoque/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 
 from .forms import EstoqueItensForm, EstoqueForm
-from .models import Estoque, EstoqueItens
+from .models import Estoque, EstoqueItens, EstoqueSaida, EstoqueEntrada
+
+from almox.produto.models import Produto
 
 
 # Create your views here.
@@ -11,23 +13,34 @@ from .models import Estoque, EstoqueItens
 
 def estoque_entrada_list(request):
     template_name = 'estoque_entrada_list.html'
-    objects = Estoque.objects.filter(movimento='e')
+    objects = EstoqueEntrada.objects.all()
     context = {'object_list': objects}
     return render(request, template_name, context)
 
 
 def estoque_entrada_detail(request, pk):
     template_name = 'estoque_entrada_detail.html'
-    obj = Estoque.objects.get(pk=pk)
+    obj = EstoqueEntrada.objects.get(pk=pk)
     context = {'object': obj}
     return render(request, template_name, context)
 
 
+# Atualiza o saldo do estoque na entrada e na saida (update)
+def dar_baixa_estoque(form):
+    # Pega os produtos a partir da instância do formulário(estoque)
+    produtos = form.estoques.all()
+    for item in produtos:
+        produto = Produto.objects.get(pk=item.produto.pk)
+        produto.estoque = item.saldo
+        produto.save()
+    print('Estoque Atualizado com sucesso.')
+
+
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
-    estoque_form = Estoque()
+    estoque_form = EstoqueEntrada()
     item_estoque_formset = inlineformset_factory(
-        Estoque,
+        EstoqueEntrada,
         EstoqueItens,
         form=EstoqueItensForm,
         extra=0,
@@ -40,10 +53,19 @@ def estoque_entrada_add(request):
         if form.is_valid() and formset.is_valid():
             form = form.save()
             formset.save()
+            # atualização dos saldos de estoque ao dar entrada e saidas
+            dar_baixa_estoque(form)
             url = 'estoque:estoque_entrada_detail'
             return HttpResponseRedirect(resolve_url(url, form.pk))
     else:
         form = EstoqueForm(instance=estoque_form, prefix='main')
         formset = item_estoque_formset(instance=estoque_form, prefix='estoque')
     context = {'form': form, 'formset': formset}
+    return render(request, template_name, context)
+
+
+def estoque_saida_list(request):
+    template_name = 'estoque_saida_list.html'
+    objects = EstoqueSaida.objects.all()
+    context = {'object_list': objects}
     return render(request, template_name, context)
