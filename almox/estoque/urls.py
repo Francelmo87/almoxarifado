e7906d92ac1fdcbcf61f1d8208e668927a7f530d
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-27 08:42:06 -0300

    criada a rota para detalhes da saida de estoque

--- almox/estoque/urls.py
+++ almox/estoque/urls.py
@@ -3,9 +3,12 @@ from almox.estoque import views as v
 
 app_name = 'estoque'
 
+
 urlpatterns = [
     path('', v.estoque_entrada_list, name='estoque_entrada_list'),
     path('<int:pk>/', v.estoque_entrada_detail, name='estoque_entrada_detail'),
     path('add/', v.estoque_entrada_add, name='estoque_entrada_add'),
+    # urls de saidas
     path('saida/', v.estoque_saida_list, name='estoque_saida_list'),
+    path('saida/<int:pk>/', v.estoque_saida_detail, name='estoque_saida_detail'),
 ]
--- almox/estoque/views.py
+++ almox/estoque/views.py
@@ -38,7 +38,7 @@ def dar_baixa_estoque(form):
 
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
-    estoque_form = EstoqueEntrada()
+    estoque_form = Estoque()
     item_estoque_formset = inlineformset_factory(
         EstoqueEntrada,
         EstoqueItens,
@@ -68,4 +68,11 @@ def estoque_saida_list(request):
     template_name = 'estoque_saida_list.html'
     objects = EstoqueSaida.objects.all()
     context = {'object_list': objects}
+    return render(request, template_name, context)
+
+
+def estoque_saida_detail(request, pk):
+    template_name = 'estoque_saida_detail.html'
+    obj = EstoqueSaida.objects.get(pk=pk)
+    context = {'object': obj}
     return render(request, template_name, context)
