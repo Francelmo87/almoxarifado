745baa5e6c44b13490db4867a83f432779d7c9ab
Gerência de Esgoto <francelmo87@hotmail.com>
2023-05-27 09:58:24 -0300

    criada a rota para saida de estoque

--- almox/estoque/urls.py
+++ almox/estoque/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     # urls de saidas
     path('saida/', v.estoque_saida_list, name='estoque_saida_list'),
     path('saida/<int:pk>/', v.estoque_saida_detail, name='estoque_saida_detail'),
+    path('saida/add/', v.estoque_saida_add, name='estoque_saida_add'),
 ]
